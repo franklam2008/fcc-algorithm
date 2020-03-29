886be838adf33982ea6765aaaa1852145cac87c4
franklam2008 <franklam2008@gmail.com>
2020-03-28 23:18:40 -0400

    note

--- 1-10-Finders-Keepers.js
+++ 1-10-Finders-Keepers.js
@@ -1,15 +1,10 @@
 function findElement(arr, func) {
   let num = 0;
   for(var i=0;i<arr.length;i++){
-
     if(func(arr[i])){
       return arr[i]
     }
-
   }
-
-
-
   return undefined;
 }
 function findElement(arr, func) {
