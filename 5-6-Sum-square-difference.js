f3074273a28d789efa35286ba97a7864ef9d3ca4
franklam2008 <franklam2008@gmail.com>
2020-04-01 13:47:11 -0400

    note

--- 5-6-Sum-square-difference.js
+++ 5-6-Sum-square-difference.js
@@ -30,3 +30,4 @@ Hence the difference between the sum of the squares of the first ten natural num
 
 Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 */
+
